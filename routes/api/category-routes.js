39fc1e58d7a1e5db42b0d13356eb38bcfba707cd
SaidHadad <saiddavid.hadad@gmail.com>
2021-08-04 17:14:38 -0400

    API
    updated category-routes.
    todo.- product-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,91 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"]
+    }
+  })
+  .then(categoryDB=> res.json(categoryDB))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    include: {
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"]
+    },
+    where: {
+      id: req.params.id
+    },
+  })
+  .then(categoryDB => {
+    if (!categoryDB){
+      res.status(404).json({ message: "No matching data found with this id"});
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+  .then(categoryDB => res.json(categoryDB))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body,
+    {
+      where: {
+        id: req.params.id
+      },
+      category_name: req.body.category_name
+    }
+  )
+  .then(categoryDB => {
+    if (!categoryDB){
+      res.status(404).json({ message: "No matching data found with this id"});
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categoryDB => {
+    if (!categoryDB){
+      res.status(404).json({ message: "No matching data found with this id"});
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
